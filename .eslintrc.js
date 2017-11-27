f6e00752f3156bf4b9ad9bf1b3e3ba16cc6a4e7a
CMoncur <CMoncur@gmail.com>
2017-11-26 19:16:43 -0800

    eslintrc base

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+      "arrow-parens": [ "error", "always" ],
+      "comma-dangle": [
+        "error",
+        {
+          "arrays": "never",
+          "objects": "always-multiline",
+          "imports": "always-multiline",
+          "exports": "always-multiline",
+          "functions": "ignore"
+        }
+      ],
+      "indent": [ "error", 2 ],
+      "linebreak-style": [ "error", "unix" ],
+      "max-len": [ "error", 80 ],
+      "no-await-in-loop": [ "error" ],
+      "no-console": [
+        "error",
+        {
+          "allow": [ "error", "log", "warn" ]
+        }
+      ],
+      "no-unused-vars": [
+        "error",
+        {
+          "vars": "all",
+          "args": "all"
+        }
+      ],
+      "prefer-const": [ "error" ],
+      "quotes": [ "error", "double" ],
+      "semi": [ "error", "never" ]
+    }
+};
