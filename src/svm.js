fbd31575038084352d3a0eccd8c12ba5190aa415
CMoncur <CMoncur@gmail.com>
2017-12-10 16:29:57 -0800

    Further cleanup

--- src/svm.js
+++ src/svm.js
@@ -95,8 +95,8 @@ module.exports = class Svm {
 
       // First heuristic
       else {
-        this.alpha.forEach((a_i, i) => {
-          if (a_i !== 0 && a_i !== this.c) {
+        this.alpha.forEach((ai, i) => {
+          if (ai !== 0 && ai !== this.c) {
             changed += this.examine(i)
           }
         })
@@ -114,42 +114,40 @@ module.exports = class Svm {
     this.trained = true
   }
 
-  examine (i_2) {
-    // TODO: Make these consts and pass them along to the step function
-    // so that we don't have to store them globally in the class.
-    this.y_2 = this.y[i_2]
-    this.x_2 = this.x[i_2]
-    this.a_2 = this.alpha[i_2]
-    this.e_2 = this.cachedError(i_2)
+  examine (i2) {
+    this.y2 = this.y[i2]
+    this.x2 = this.x[i2]
+    this.a2 = this.alpha[i2]
+    this.e2 = this.cachedError(i2)
 
-    const nonZeroNonCAlpha = [], r_2 = this.e_2 * this.y_2
+    const nonZeroNonCAlpha = [], r2 = this.e2 * this.y2
 
     if (
-      // KKT conditions -- make UTIL function
-      (r_2 < -this.tolerance && this.a_2 < this.c) ||
-      (r_2 > this.tolerance && this.a_2 > 0)
+      // KKT conditions
+      (r2 < -this.tolerance && this.a2 < this.c) ||
+      (r2 > this.tolerance && this.a2 > 0)
     ) {
       // Tally non-zero and non-C alphas
-      this.alpha.forEach((a_i, i) => {
-        if (a_i > 0 && a_i < this.c) {
+      this.alpha.forEach((ai, i) => {
+        if (ai > 0 && ai < this.c) {
           nonZeroNonCAlpha.push(i)
         }
       })
 
       // Second heuristic, attempt 1
       if (nonZeroNonCAlpha.length > 1) {
-        let i_1, max = 0, stepSize
+        let i1, max = 0, stepSize
 
         nonZeroNonCAlpha.forEach((j) => {
-          stepSize = Math.abs((this.error[j] - this.y[j]) - this.e_2)
+          stepSize = Math.abs((this.error[j] - this.y[j]) - this.e2)
 
           if (stepSize > max) {
             max = stepSize
-            i_1 = j
+            i1 = j
           }
         })
 
-        if (Util.isNum(i_1) && this.step(i_1, i_2)) {
+        if (Util.isNum(i1) && this.step(i1, i2)) {
           return 1
         }
       }
@@ -158,14 +156,14 @@ module.exports = class Svm {
       const randPartialSequence = Util.randSequence(nonZeroNonCAlpha)
 
       // .some will cease execution once the first truthy value is called back
-      if (randPartialSequence.some((i) => this.step(i, i_2))) {
+      if (randPartialSequence.some((i) => this.step(i, i2))) {
         return 1
       }
 
       // Second heuristic, attempt 3
       // eslint-disable-next-line no-unused-vars
-      const fullSequence = Array(this.m).fill(0).reduce((x, xs, idx) => {
-        x.push(idx)
+      const fullSequence = Array(this.m).fill(0).reduce((x, xs, i) => {
+        x.push(i)
 
         return x
       }, [])
@@ -173,7 +171,7 @@ module.exports = class Svm {
       const randFullSequence = Util.randSequence(fullSequence)
 
       // .some will cease execution once the first truthy value is called back
-      if (randFullSequence.some((i) => this.step(i, i_2))) {
+      if (randFullSequence.some((i) => this.step(i, i2))) {
         return 1
       }
     }
@@ -182,119 +180,105 @@ module.exports = class Svm {
     return 0
   }
 
-  step(i_1, i_2) {
+  step(i1, i2) {
     // Occurs in rare cases if all step sizes from second heuristic are < 0
-    if (!Util.isNum(i_1)) {
+    if (!Util.isNum(i1)) {
       return false
     }
 
     // If both indices are the same, don't step
-    if (i_1 === i_2) {
+    if (i1 === i2) {
       return false
     }
 
-    // TODO: Make y_2, a_2, etc... parameters so that they don't have to be
-    // stored globally in the class
     const
-      a_1 = this.alpha[i_1],
-      y_1 = this.y[i_1],
-      x_1 = this.x[i_1],
-      e_1 = this.cachedError(i_1),
-      s = y_1 * this.y_2
+      a1 = this.alpha[i1],
+      y1 = this.y[i1],
+      x1 = this.x[i1],
+      e1 = this.cachedError(i1),
+      s = y1 * this.y2,
+      k11 = this.kern(x1, x1),
+      k12 = this.kern(x1, this.x[i2]),
+      k22 = this.kern(this.x[i2], this.x[i2]),
+      eta = k11 + k22 - 2 * k12
 
-    let a_2New, l, h
+    let a2New, l, h
 
-    if (y_1 === this.y_2) {
-      l = Math.max(0, this.a_2 + a_1 - this.c)
-      h = Math.min(this.c, this.a_2 + a_1)
+    if (y1 === this.y2) {
+      l = Math.max(0, this.a2 + a1 - this.c)
+      h = Math.min(this.c, this.a2 + a1)
     }
 
     else {
-      l = Math.max(0, this.a_2 - a_1)
-      h = Math.min(this.c, this.c + this.a_2 - a_1)
+      l = Math.max(0, this.a2 - a1)
+      h = Math.min(this.c, this.c + this.a2 - a1)
     }
 
     if (l === h) {
       return false
     }
 
-    // TODO: Combine this with const block above
-    // TODO: Make these read k_11, k_12, k_22
-    const
-      k11 = this.kern(x_1, x_1),
-      k12 = this.kern(x_1, this.x[i_2]),
-      k22 = this.kern(this.x[i_2], this.x[i_2]),
-      // Move this calculation to Formula
-      eta = k11 + k22 - 2 * k12
-
     if (eta > 0) {
-      // Move this calculation to Formula
-      a_2New = this.a_2 + this.y_2 * (e_1 - this.e_2) / eta
+      a2New = this.a2 + this.y2 * (e1 - this.e2) / eta
 
-      if (a_2New < l) {
-        a_2New = l
+      if (a2New < l) {
+        a2New = l
       }
 
-      else if (a_2New > h) {
-        a_2New = h
+      else if (a2New > h) {
+        a2New = h
       }
     }
 
     else {
-      // TODO: f_1, f_2, l_1, h_1, psi_L, psi_H should all be moved to Formula
-      const f_1 = y_1 * (e_1 + this.b) - a_1 * k11 - s * this.a_2 * k12
+      const
+        f1 = y1 * (e1 + this.b) - a1 * k11 - s * this.a2 * k12,
+        f2 = this.y2 * (this.e2 + this.b) - s * a1 * k12 - this.a2 * k22,
+        l1 = a1 + s * (this.a2 - l),
+        h1 = a1 + s * (this.a2 - h)
 
-      const f_2 = this.y_2 * (this.e_2 + this.b) - s * a_1 * k12 - this.a_2
-        * k22
+      const psiL = l1 * f1 + l * f2 + 0.5 * Math.pow(l1, 2) * k11
+        + 0.5 * Math.pow(l, 2) * k22 + s * l * l1 * k12
 
-      const l_1 = a_1 + s * (this.a_2 - l)
+      const psiH = h1 * f1 + h * f2 + 0.5 * Math.pow(h1, 2) * k11
+        + 0.5 * Math.pow(h, 2) * k22 + s * h * h1 * k12
 
-      const h_1 = a_1 + s * (this.a_2 - h)
-
-      const psi_l = l_1 * f_1 + l * f_2 + 0.5 * Math.pow(l_1, 2) * k11
-        + 0.5 * Math.pow(l, 2) * k22 + s * l * l_1 * k12
-
-      const psi_h = h_1 * f_1 + h * f_2 + 0.5 * Math.pow(h_1, 2) * k11
-        + 0.5 * Math.pow(h, 2) * k22 + s * h * h_1 * k12
-
-      if (psi_l < psi_h - this.tolerance) {
-        a_2New = l
+      if (psiL < psiH - this.tolerance) {
+        a2New = l
       }
 
-      else if (psi_l > psi_h + this.tolerance) {
-        a_2New = h
+      else if (psiL > psiH + this.tolerance) {
+        a2New = h
       }
 
       else {
-        a_2New = this.a_2
+        a2New = this.a2
       }
     }
 
-    // TODO make this a formula
-    const changeNegligible = Math.abs(a_2New - this.a_2) < this.tolerance
-      * (a_2New + this.a_2 + this.tolerance)
+    const changeNegligible = Math.abs(a2New - this.a2) < this.tolerance
+      * (a2New + this.a2 + this.tolerance)
 
     if (changeNegligible) {
       return false
     }
 
     // Solve for B
-    // TODO: Make formulae for solving for B
-    const a_1New = a_1 + s * (this.a_2 - a_2New)
-
     let bNew
 
-    const b_1 = e_1 + y_1 * (a_1New - a_1) * k11 + this.y_2
-      * (a_2New - this.a_2) * k12 + this.b
+    const a1New = a1 + s * (this.a2 - a2New)
 
-    const b_2 = this.e_2 + y_1 * (a_1New - a_1) * k12 + this.y_2
-      * (a_2New - this.a_2) * k22 + this.b
+    const b_1 = e1 + y1 * (a1New - a1) * k11 + this.y2
+      * (a2New - this.a2) * k12 + this.b
 
-    if (0 < a_1 && a_1 < this.c) {
+    const b_2 = this.e2 + y1 * (a1New - a1) * k12 + this.y2
+      * (a2New - this.a2) * k22 + this.b
+
+    if (0 < a1 && a1 < this.c) {
       bNew = b_1
     }
 
-    else if (0 < this.a_2 && this.a_2 < this.c) {
+    else if (0 < this.a2 && this.a2 < this.c) {
       bNew = b_2
     }
 
@@ -307,34 +291,31 @@ module.exports = class Svm {
     this.b = bNew
 
     // Solve for W
-    // TODO: Make this a formula function
-
-    // TODO Give these better names and make them a formula
-    const y1a1a1x1 = x_1.map((v) => {
-      return y_1 * (a_1New - a_1) * v
+    const wlm1 = x1.map((v) => {
+      return y1 * (a1New - a1) * v
     })
 
-    const y2a2a2x2 = this.x_2.map((v) => {
-      return this.y_2 * (a_2New - this.a_2) * v
+    const wlm2 = this.x2.map((v) => {
+      return this.y2 * (a2New - this.a2) * v
     })
 
-    this.w = Formula.vectorSum(this.w, Formula.vectorSum(y1a1a1x1, y2a2a2x2))
+    const lm1 = y1 * (a1New - a1), lm2 = this.y2 * (a2New - this.a2)
 
-    // TODO: Make a formula function for lagrange multipliers
-    const lm1 = y_1 * (a_1New - a_1), lm2 = this.y_2 * (a_2New - this.a_2)
+    this.w = Formula.vectorSum(this.w, Formula.vectorSum(wlm1, wlm2))
 
+    // Update error cache
     // eslint-disable-next-line no-unused-vars
     Array(this.m).fill(0).forEach((mi, i) => {
       if (0 < this.alpha[i] && this.alpha[i] < this.c) {
-        this.error[i] += lm1 * this.kern(x_1, this.x[i]) + lm2
-          * this.kern(this.x_2, this.x[i]) - bChange
+        this.error[i] += lm1 * this.kern(x1, this.x[i]) + lm2
+          * this.kern(this.x2, this.x[i]) - bChange
       }
     })
 
-    this.error[i_1] = 0
-    this.error[i_2] = 0
-    this.alpha[i_1] = a_1New
-    this.alpha[i_2] = a_2New
+    this.error[i1] = 0
+    this.error[i2] = 0
+    this.alpha[i1] = a1New
+    this.alpha[i2] = a2New
 
     return true
   }
