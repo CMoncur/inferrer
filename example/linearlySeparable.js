0bde1ee3954616eeb4c5bb50f88a61633d3c3389
CMoncur <CMoncur@gmail.com>
2017-12-25 22:12:08 -0800

    Adjust training data for linearly separable example

--- example/linearlySeparable.js
+++ example/linearlySeparable.js
@@ -1,29 +1,28 @@
 const Inferrer = require("../src/svm")
 
-const trainingData = {
-  input: [
-    // "1" values
-    [ 4, 6 ],
-    [ 5, 4 ],
-    [ 8, 2 ],
-    [ 2, 9 ],
-    [ 6, 6 ],
-    [ 3, 7 ],
-    [ 5, 8 ],
-    [ 7, 4 ],
-    // "-1" values
-    [ 4, 2 ],
-    [ 6, 0 ],
-    [ 2, 4 ],
-    [ 2, 6 ],
-    [ 3, 3 ],
-    [ 2, 3 ],
-    [ 3, 1 ],
-    [ 1, 7 ]
-  ],
+const trainingData = [
+  // "1" values
+  { input: [ 4, 6 ], classification: 1 },
+  { input: [ 5, 4 ], classification: 1 },
+  { input: [ 8, 2 ], classification: 1 },
+  { input: [ 2, 9 ], classification: 1 },
+  { input: [ 6, 6 ], classification: 1 },
+  { input: [ 3, 7 ], classification: 1 },
+  { input: [ 5, 8 ], classification: 1 },
+  { input: [ 7, 4 ], classification: 1 },
+  // "-1" values
+  { input: [ 4, 2 ], classification: -1 },
+  { input: [ 6, 0 ], classification: -1 },
+  { input: [ 2, 4 ], classification: -1 },
+  { input: [ 2, 6 ], classification: -1 },
+  { input: [ 3, 3 ], classification: -1 },
+  { input: [ 2, 3 ], classification: -1 },
+  { input: [ 3, 1 ], classification: -1 },
+  { input: [ 1, 7 ], classification: -1 }
+]
 
-  classification: [ 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1 ],
-}
+//   classification: [ 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+// }
 
 const LinearSvm = new Inferrer()
 
